bf7941dd908ec8e951bc454b20ef50caeac03825
Abhi singh <124481150+abhisinghcpr@users.noreply.github.com>
2025-02-15 12:29:12 +0530

    food order

--- food_gao/lib/main.dart
+++ food_gao/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:food_gao/services/firebase_services.dart';
+import 'package:food_gao/views/auth/PersonalDetailsScreen.dart';
 import 'package:food_gao/views/home/splash_screen.dart';
 import 'package:provider/provider.dart';
 import 'firebase_options.dart';
@@ -30,7 +31,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: SplashScreen(),
+        home: RegisterScreen(),
       ),
     );
   }
--- food_gao/lib/views/home/favoriteScreen.dart
+++ food_gao/lib/views/home/favoriteScreen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:food_gao/utils/app_color.dart';
 
 class FavoritesScreen extends StatelessWidget {
